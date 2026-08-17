d328c74f3d3467ae95264f0fb289e3c404005379
Sagar Mandal <69377939+mandalsagar228@users.noreply.github.com>
2026-08-17 23:44:55 +0545

    added router node and llm models

--- backend/services/agent/graph/router.js
+++ backend/services/agent/graph/router.js
@@ -1,1 +1,58 @@
-export const router = async (params) => {};
+import { getModel } from "../config/llmModels";
+
+export const router = async (state) => {
+  const llm = await getModel("router");
+  const prompt = `You are an agent router. 
+    
+    available agents:
+        -chat,
+        -search
+        -coding,
+        -pdf,
+        -ppt,
+        -vision
+        
+        
+        Rules:
+        chat:
+            General conversation,
+            explanation,
+            Learning,questions
+        search:
+            Current events,
+            latest information,
+            news, 
+            recent development,
+            internet lookup,
+            
+        coding:
+            Generate code,
+            debug code,
+            build projects,
+            architecture,
+            API development
+            
+        pdf:
+            questions about generate pdf,
+            Document context,
+            
+        ppt:
+            Questions about generate pdf,
+            ppt context,
+            
+        vision:
+            Generate image,
+            create image,
+        
+        
+        user query:
+            ${state.prompt}
+                `;
+
+  const response = await llm.invoke(prompt);
+
+  return {
+    ...state,
+    agent: response.content.trim().toLowerCase(),
+  };
+};
